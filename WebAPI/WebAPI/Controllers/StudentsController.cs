23f44222a5d311e18d7c5bf5546db6383b6054d1
DP <dp@sensus.dk>
2018-02-28 15:58:29 +0100

    More practice on Web APIs

---
+++ WebAPI/WebAPI/Controllers/StudentsController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    //[RoutePrefix("api/students")]
+    public class StudentsController : ApiController
+    {
+        static List<Student> students = new List<Student>()
+        {
+            new Student() { Id= 1, Name = "Tome" },
+            new Student() { Id = 2, Name = "Sam" },
+            new Student() { Id = 3, Name = "John" }
+        };
+
+        //public HttpResponseMessage Get()
+        //{
+        //    return Request.CreateResponse(HttpStatusCode.OK, students);
+        //}
+
+        public IHttpActionResult Get()
+        {
+            return Ok(students);
+        }
+
+        //public HttpResponseMessage Get(int id)
+        //{
+        //    var student = students.FirstOrDefault(s => s.Id == id);
+
+        //    if (student == null)
+        //    {
+        //        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Student not found");
+        //    }
+        //    return Request.CreateResponse(HttpStatusCode.OK, student);
+        //}
+        public IHttpActionResult Get(int id)
+        {
+            var student = students.FirstOrDefault(s => s.Id == id);
+
+            if (student == null)
+            {
+                return Content(HttpStatusCode.NotFound, "Student not found");
+            }
+            return Ok(student);
+        }
+
+
+        //// Override the RoutePrefix
+        //[Route("~/api/teachers")]
+        //public IEnumerable<Teacher> GetTeachers()
+        //{
+        //    List<Teacher> teachers = new List<Teacher>()
+        //    {
+        //        new Teacher() { Id = 1, Name = "Rob" },
+        //        new Teacher() { Id = 1, Name = "Mike" },
+        //        new Teacher() { Id = 1, Name = "Mary" }
+        //    };
+
+        //    return teachers;
+        //}
+        //[Route("")]
+        //public IEnumerable<Student> Get()
+        //{
+        //    return students;
+        //}
+
+        //// Route Contraint
+        //// Only positive value greater than 0 and less than 3
+        //[Route("{id:int:min(1):max(3)}")]
+        //public Student Get(int id)
+        //{
+        //    return students.FirstOrDefault(s => s.Id == id);
+        //}
+
+        ////[Route("{id:int}", Name = "GetStudentById")]
+        ////public Student Get(int id)
+        ////{
+        ////    return students.FirstOrDefault(s => s.Id == id);
+        ////}
+
+        ////public HttpResponseMessage Post(Student student)
+        ////{
+        ////    students.Add(student);
+        ////    var response = Request.CreateResponse(HttpStatusCode.Created);
+        ////    response.Headers.Location = new Uri(Url.Link("GetStudentById", new { id= student.Id}));
+        ////    return response;
+        ////}
+
+        //[Route("{name:alpha}")]
+        //public Student Get(string name)
+        //{
+        //    return students.FirstOrDefault(s => s.Name.ToLower() == name.ToLower());
+        //}
+
+        //[Route("{id}/courses")]
+        //public IEnumerable<string> GetStudentCourses(int id)
+        //{
+        //    if (id == 1)
+        //        return new List<String>() { "C#", "ASP.NET", "SQL Server" };
+        //    else if (id == 2)
+        //        return new List<string>() { "ASP.NET Web API", "C#", "SQL Server" };
+        //    else
+        //        return new List<string> { "Bootstrap", "JQuery", "AngularJs" };
+        //}
+    }
+}
---
+++ WebAPIVersioning/WebAPIVersioning/App_Start/WebApiConfig.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.Owin.Security.OAuth;
+using Newtonsoft.Json.Serialization;
+using System.Web.Http.Dispatcher;
+using WebAPIVersioning.Custom;
+using System.Net.Http.Headers;
+
+namespace WebAPIVersioning
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            // Configure Web API to use only bearer token authentication.
+            config.SuppressDefaultHostAuthentication();
+            config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            // Only for Web API versioning using custom media types
+            config.Formatters.JsonFormatter.SupportedMediaTypes
+                .Add(new MediaTypeHeaderValue("application/vnd.pragimtech.students.v1+json"));
+            config.Formatters.JsonFormatter.SupportedMediaTypes
+                .Add(new MediaTypeHeaderValue("application/vnd.pragimtech.students.v2+json"));
+            config.Formatters.XmlFormatter.SupportedMediaTypes
+                .Add(new MediaTypeHeaderValue("application/vnd.pragimtech.students.v1+xml"));
+            config.Formatters.XmlFormatter.SupportedMediaTypes
+                .Add(new MediaTypeHeaderValue("application/vnd.pragimtech.students.v2+xml"));
+            ///////
+
+            // Web API versioning by URI
+            //config.Routes.MapHttpRoute(
+            //    name: "Version1",
+            //    routeTemplate: "api/v1/students/{id}",
+            //    defaults: new { id = RouteParameter.Optional, controller = "StudentsV1" }
+            //);
+
+            //config.Routes.MapHttpRoute(
+            //    name: "Version2",
+            //    routeTemplate: "api/v2/students/{id}",
+            //    defaults: new { id = RouteParameter.Optional, controller = "StudentsV2" }
+            //);
+
+
+            // Versioning by Query String
+            config.Routes.MapHttpRoute(
+                name: "DefaultRoute",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            config.Services.Replace(typeof(IHttpControllerSelector), new CustomControllerSelector(config));
+        }
+    }
+}
